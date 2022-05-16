ec069f4934ca410f176c2b2208a20420067cfe66
Dodo0303 <dodo0303@icloud.com>
2022-05-16 12:50:11 +0200

    Styling

--- src/main/java/it/polimi/tiw/controllers/GoToRegistration.java
+++ src/main/java/it/polimi/tiw/controllers/GoToRegistration.java
@@ -18,7 +18,7 @@ import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 /**
  * Servlet implementation class GoToRegistration
  */
-@WebServlet("/GoToRegistration")
+@WebServlet("/Registration")
 public class GoToRegistration extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private TemplateEngine templateEngine;
